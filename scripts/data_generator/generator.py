48bfcac3e84210fb395ab51545a265301503df4e
viveksingh12061999 <viveksingh12061999@gmail.com>
2026-07-17 12:30:36 +0530

    feat(generator): build source data generation framework

--- scripts/data_generator/generator.py
+++ scripts/data_generator/generator.py
@@ -1,15 +1,57 @@
 """
-Master Data Generator.
+Master Data Generator
 
-Responsible for executing all
-data generation methods.
-
+Responsible for running all source data generation jobs
+for the Retail Data Warehouse project.
 """
+
+#
+from config.settings import NUM_CUSTOMERS
+
+print(f"Customers to generate: {NUM_CUSTOMERS}")
+
+
+#
+from config.settings import (
+    SALES_DIR,
+    CUSTOMER_DIR,
+    PRODUCT_DIR,
+)
+from scripts.data_generator.utils import create_directory
+
+create_directory(SALES_DIR)
+create_directory(CUSTOMER_DIR)
+create_directory(PRODUCT_DIR)
+
+
+def generate_customers():
+    print("Generating customers...")
+
+
+def generate_products():
+    print("Generating products...")
+
+
+def generate_sales():
+    print("Generating sales...")
 
 
 def main():
+    print("=" * 60)
     print("Retail Data Warehouse")
-    print("Source Data Generator")
+    print("Source Data Generation")
+    print("=" * 60)
+
+    # Create required directories
+    create_directory(CUSTOMER_DIR)
+    create_directory(PRODUCT_DIR)
+    create_directory(SALES_DIR)
+
+    generate_customers()
+    generate_products()
+    generate_sales()
+
+    print("\nData generation completed successfully.")
 
 
 if __name__ == "__main__":
--- scripts/data_generator/utils.py
+++ scripts/data_generator/utils.py
@@ -13,3 +13,35 @@ def create_directory(path: Path):
     """
     if not path.exists():
         path.mkdir(parents=True, exist_ok=True)
+
+
+def current_date() -> str:
+    """
+    Return today's date in format "dd-mm-yyyy
+    """
+    return datetime.now().strftime("%d-%m-%Y")
+
+
+def current_timestamp() -> str:
+    """
+    Return the current timestamp in ISO format.
+    """
+    return datetime.now().isoformat()
+
+
+def generate_uuid() -> str:
+    """
+    Generate a unique string
+    """
+    return str(uuid.uuid4())
+
+
+def calculate_revenue(quantity: int, unit_price: Decimal, discount: Decimal) -> Decimal:
+    """
+    Calculate sales revenue.
+
+    Formula:
+    Revenue = (Quantity * Unit Price)- Discount
+
+    """
+    return (unit_price * quantity) - discount
